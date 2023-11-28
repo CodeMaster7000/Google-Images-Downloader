267cfc625fcd0a10c3d399231b4512e8292e3b5b
CodeMaster7000 <CodeMaster7K@gmail.com>
2023-11-28 22:52:35 +0000

    Add files via upload

---
+++ Google_Images_Downloader.py
@@ -0,0 +1,31 @@
+from google_images_download import google_images_download 
+response = google_images_download.googleimagesdownload() 
+search_queries =
+[
+# Input search queries here.
+]
+
+def downloadimages(query):
+    arguments = {"keywords": query,
+                 "format": "jpg",
+                 "limit":4,
+                 "print_urls":True,
+                 "size": "medium",
+                 "aspect_ratio":"panoramic"}
+    try:
+        response.download(arguments)  
+
+    except FileNotFoundError: 
+        arguments = {"keywords": query,
+                     "format": "jpg",
+                     "limit":4,
+                     "print_urls":True, 
+                     "size": "medium"}
+
+        try:
+            response.download(arguments) 
+        except:
+            pass
+ for query in search_queries:
+    downloadimages(query) 
+    print() 
